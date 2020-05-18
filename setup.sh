b563d5d6c771940193633e40e08518650e8676e9
marissawatkins <mjw2d4@mail.missouri.edu>
2020-05-18 16:12:14 -0700

    added Procfile

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-export DATABASE_URL='postgres://postgres@localhost:5432/capstone'
+export DATABASE_URL='postgres://srjodxxjjrwovb:7db52f15caf9edac205e47e35d7457983868a716fcbf1e3ef1b6549e4689dcaf@ec2-52-70-15-120.compute-1.amazonaws.com:5432/ddbhr7mk4qstd2'
 export SQLALCHEMY_TRACK_MODIFICATIONS=False
 export AUTH0_DOMAIN="mwcapstone.auth0.com"
 export API_AUDIENCE='capstone'
