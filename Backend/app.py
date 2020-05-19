5bf7539dbbe5c273b8fee56daa2ca369218b1a9b
marissawatkins <mjw2d4@mail.missouri.edu>
2020-05-18 17:20:07 -0700

    trying to get heroku running

--- Backend/app.py
+++ Backend/app.py
@@ -192,7 +192,7 @@ def create_app(test_config=None):
 APP = create_app()
 
 if __name__ == '__main__':
-    # port = int(os.environ.get("PORT", 5000))  
+    port = int(os.environ.get("PORT", 5000))  
     APP.run(host='0.0.0.0', port=8080, debug=True)
 
 
