6ad560b1aeed13b69c31a9456b1251c06743d43f
marissawatkins <mjw2d4@mail.missouri.edu>
2020-05-18 18:02:55 -0700

    upgrade heroku

--- Backend/app.py
+++ Backend/app.py
@@ -190,10 +190,10 @@ def create_app(test_config=None):
 
     return app
 
-APP = create_app()
+app = create_app()
 
 if __name__ == '__main__':
     # port = int(os.environ.get("PORT", 5000))  
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
 
