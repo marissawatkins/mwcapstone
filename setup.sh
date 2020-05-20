08f92a640dce21b939bb9b188d53510710aa9fb1
marissawatkins <mjw2d4@mail.missouri.edu>
2020-05-19 17:03:44 -0700

    update

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 export DATABASE_URL='postgres://srjodxxjjrwovb:7db52f15caf9edac205e47e35d7457983868a716fcbf1e3ef1b6549e4689dcaf@ec2-52-70-15-120.compute-1.amazonaws.com:5432/ddbhr7mk4qstd2'
+# export DATABASE_URL='postgres://postgres:password@localhost:5432/capstone'
 export SQLALCHEMY_TRACK_MODIFICATIONS=False
 export AUTH0_DOMAIN="mwcapstone.auth0.com"
 export API_AUDIENCE='capstone'
@@ -10,5 +11,6 @@ export CLIENT_ID='mJLgnqtDiw1gly6qpl8ScB17fiJ5wILh'
 export FLASK_APP=app.py
 export FLASK_ENV=development
 export SQLALCHEMY_DATABASE_URI='postgres://srjodxxjjrwovb:7db52f15caf9edac205e47e35d7457983868a716fcbf1e3ef1b6549e4689dcaf@ec2-52-70-15-120.compute-1.amazonaws.com:5432/ddbhr7mk4qstd2'
+# export SQLALCHEMY_DATABASE_URI='postgres://postgres:password@localhost:5432/capstone'
 LOG_LEVEL=DEBUG
 
